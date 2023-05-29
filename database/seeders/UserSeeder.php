1ed4730a89af87ba54f10c53d32798ff841a21a9
Argi Fatur <argifatur@gmail.com>
2023-05-30 07:08:54 +0800

    adding some rule & seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'full_name' => 'Admin',
+            'username' => 'admin',
+            'email' => 'faturrr145@gmail.com',
+            'password' => bcrypt('admin123')
+        ]);
+    }
+}
